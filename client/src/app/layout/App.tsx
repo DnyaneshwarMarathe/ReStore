133a18ef6c21d244f28e5533792554b95863b937
AP\maratdny <dnyaneshwar.marathe@tietoevry.com>
2022-07-21 16:04:38 +0530

    Enf of Section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,16 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,6 +30,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
@@ -35,6 +40,8 @@ function App() {
         <Route path='/catalog/:id' element={<ProductDetails/>} />
         <Route path='/about' element={<AboutPage/>} />
         <Route path='/contact' element={<ContactPage/>} />
+        <Route path='/server-error' element={<ServerError/>} />
+        <Route element={<NotFound/>} />
         </Routes>
         
       </Container>
